1a4a80ddcc1b4b36a098b6d15a9a3a1089e47e2a
Kyeongmo Yang <gaeng1107@gmail.com>
2025-11-18 13:37:47 +0900

    Create 530.Minimum_Absolute_Difference_in_BST.swift

---
+++ LeetCode/530.Minimum_Absolute_Difference_in_BST.swift
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func getMinimumDifference(_ root: TreeNode?) -> Int {
+        var prev: Int? = nil
+        var minDiff = 100001
+
+        func inorder(_ node: TreeNode?) {
+            guard let node else {
+                return
+            }
+
+            inorder(node.left)
+
+            if let prev {
+                minDiff = min(minDiff, node.val - prev)
+            }
+
+            prev = node.val
+
+            inorder(node.right)
+        }
+
+        inorder(root)
+        return minDiff
+    }
+}
