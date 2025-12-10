a60de6c1b8144dd0902d200830292731a86a536d
Kyeongmo Yang <gaeng1107@gmail.com>
2025-12-10 22:05:27 +0900

    Create 28 Find the Index of the First Occurrence in a String.swift

---
+++ LeetCode/28 Find the Index of the First Occurrence in a String.swift
@@ -0,0 +1,8 @@
+class Solution {
+    func strStr(_ haystack: String, _ needle: String) -> Int {
+        guard let range = haystack.range(of: needle) else {
+            return -1 
+            }
+        return haystack.distance(from: haystack.startIndex, to: range.lowerBound)
+    }
+}
