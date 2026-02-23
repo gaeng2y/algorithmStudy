057ebdb107167c796dbdc84b0a72a78613814ab0
Kyeongmo Yang <gaeng1107@gmail.com>
2026-02-23 13:50:50 +0900

    Create 0338. Counting Bits.swift

---
+++ LeetCode/0338. Counting Bits.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func countBits(_ n: Int) -> [Int] {
+        var ans = Array(repeating: 0, count: n + 1)
+        if n == 0 { return ans }
+
+        for i in 1...n {
+            ans[i] = ans[i >> 1] + (i & 1)
+        }
+        return ans
+    }
+}
